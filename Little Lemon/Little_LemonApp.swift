5dace7b734cff38befab8243a0d3a72a63541cce
Anton Fischer <anton.fischer@sap.com>
2024-12-06 13:34:18 +0100

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Fischer, Anton on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
