41a6bafb71c50fb9744e0f3f36b9e8022967f5e1
Anton Fischer <anton.fischer@sap.com>
2024-12-08 14:11:47 +0100

    Added Core Data support and Dish entity, fetching menu from server, decoding into struct, adding to core data, displaying results in menu view

---
+++ Little Lemon/model/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Little Lemon
+//
+//  Created by Fischer, Anton on 08/12/2024.
+//
+
+import Foundation
+
+struct MenuItem: Decodable {
+    let title: String
+    let image: String
+    let price: String
+}
---
+++ Little Lemon/model/MenuList.swift
@@ -0,0 +1,12 @@
+//
+//  MenuList.swift
+//  Little Lemon
+//
+//  Created by Fischer, Anton on 08/12/2024.
+//
+
+import Foundation
+
+struct MenuList: Decodable {
+    let menu: [MenuItem]
+}
